8b7c1a3e2939b71a066980847a39fc3a35ef3077
Dasha <tcvetkova.daria22@gmail.com>
2023-02-16 19:06:59 +0200

    homework

---
+++ index15.js
@@ -0,0 +1,26 @@
+/* let message = "Я учу JavaScript!" // Создайте страницу, подключите скрипт. Создайте функцию, которая выведет сообщение в консоли «Я учу JavaScript!».
+console.log (message); */
+
+/*let message = "Я учу JavaScript!"
+alert(message); */
+
+/* function showMessage () {
+alert ("Я учу JavaScript!");
+}
+showMessage (); */
+
+
+ /* console.log ("Я учу JavaScript!"); */
+
+
+
+
+function changeImg() {
+    let image = document.getElementById('elem-img1');
+    image.src = '/JS/HomeWorks/assets/images/kittie1.jpg';
+}
+
+function changeImg2() {
+    let image = document.getElementById('elem-img2');
+    image.src = '/JS/HomeWorks/assets/images/kittie2.jpg';
+}
